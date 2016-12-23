54519f46b471a00429e5144de7cd19c16ffe1b2c
Matt <Matt.w.stelmach@gmail.com>
2016-12-23 10:20:16 -1000

    Comments and formatting

--- restartStravaScraper.py
+++ restartStravaScraper.py
@@ -3,6 +3,8 @@
 Created on Tue Jul 19 20:53:01 2016
 
 @author: MStelmach
+This file uses decimal degrees from WGS 84 which will lead to distorted squares.
+This was sufficient for the use created, verify your use case.
 """
 
 import polyline
@@ -16,33 +18,33 @@ import sys
 
 starttime = datetime.datetime.now()
 
+#++++++++++++++++++++++++++++++++++++++++++++++++
+#Set your previous working directory to resume
 workingdir = 'H:\MyDocuments\UW\CapstoneProject\Scripts\Working'
-#segments = workingdir + '\stravasegments20160721.txt'#ADD YOUR PREVIOUS OUTPUT
+
 previous_JSON = workingdir + '\lastboundingbox.txt'#ADD YOUR PREVIOUS TRACKING OUTPUT
 
 tempfile = workingdir + '\stravasegments20160721.txt'  #ADD YOUR PREVIOUS OUTPUT
-
+#++++++++++++++++++++++++++++++++++++++++++++++++
 jsonreader = open(previous_JSON, 'r')
 
 reader = jsonreader.read()
 
 print reader, type(reader)
 
+#Read from the previous file into memory
 stats = simplejson.loads(reader)
-#lastboundingbox = boundreader.readline().replace('\n','')
-lastrunstart = stats['StartTime'] #boundreader.readline().replace('\n','')
-lastrunend = stats['CurrentTime']#boundreader.readline().replace('\n','')
-currentbox = stats['currentbox']#boundreader.readline().replace('\n','')
-extent = stats['extent']#boundreader.readline().replace('\n','')
+
+#Read previous run into memory to continue.
+lastrunstart = stats['StartTime'] 
+lastrunend = stats['CurrentTime']
+currentbox = stats['currentbox']
+extent = stats['extent']
 dist = stats['dist']
-#print lastboundingbox , type (lastboundingbox)
-#print 'last run start:', lastrunstart, type (lastrunstart)
-#print 'last run end:',lastrunend, type (lastrunend)
-#print 'current box: ', currentbox, type (currentbox)
-#print 'extent:', extent, type (extent)
+
 jsonreader.close()
 
-
+#Confirm the start time of the last run
 lastend = datetime.datetime.strptime(lastrunend,'%Y-%m-%dT%H:%M:%S.%f') 
 laststart = datetime.datetime.strptime(lastrunstart,'%Y-%m-%dT%H:%M:%S.%f')
 lastruntime = lastend-laststart
@@ -51,26 +53,17 @@ deltadate = starttime.date() - lastend.date()
 deltadate = deltadate.total_seconds()/(60*60*24)
 print deltadate, 'days since last run'
 
+#Create a bounding box for the current area
 LatMin = currentbox['boxLatMin']
 LatMax = currentbox['boxLatMax']
 LonMin = currentbox['boxLonMin']
 LonMax = currentbox['boxLonMax']
+
+#Create a bounding box for the entire area
 LatMinOriginal = extent['LatMin']
 LatMaxOriginal = extent['LatMax']
 LonMinOriginal = extent['LonMin']
 LonMaxOriginal = extent['LonMax']
-
-#print LatMin,LatMax,LonMin,LonMax, dist
-
-#previoussegments = set()
-#StravaSegments = open(segments, 'r')
-
-#MBSStrava = simplejson.loads(StravaSegments.read())
-#for line in MBSStrava:
-#    previoussegments.add(MBSStrava[line]['segmentID'])
-    
-#StravaSegments.close()
-#print previoussegments
 
 
 #Stravalib client identification
@@ -78,29 +71,36 @@ AccessToken = "" #ADD YOUR ACCESS TOKEN
 client = stravalib.client.Client()
 client.access_token = AccessToken
 
-#Bounding box tracking file
+#Bounding box tracking file, allows for restarting.
 def boundfiletracker(starttime, workingdir, bounds, bbox, dist):
     
     tracker = {}
     boundfiletracker = open(workingdir + '\LastBoundingBox.txt', 'w')
     tracker['StartTime'] = starttime.isoformat()
     tracker['CurrentTime'] = datetime.datetime.now().isoformat()
-    tracker['currentbox'] = {'boxLatMin':bounds[0], 'boxLonMin':bounds[1], 'boxLatMax':bounds[2], 'boxLonMax':bounds[3]}
+    tracker['currentbox'] = {'boxLatMin':bounds[0],
+                             'boxLonMin':bounds[1],
+                             'boxLatMax':bounds[2],
+                             'boxLonMax':bounds[3]}
     tracker['extent'] = bbox
     tracker['dist'] = dist
     boundfiletracker.write(simplejson.dumps(tracker))
     boundfiletracker.close()
     
-# create an empty set for segment ID numbers, and counters for row, column, and count of empty lists
+# create an empty set for segment ID numbers, 
+# and counters for row, column, and count of empty lists
 segmentlist = set()
 emptylistcount =0
 row =1
 count = 0
 calls = 0
 
-
+#Return the length of the bounding box sides
 latmax = abs(LatMaxOriginal-LatMinOriginal)
 lonmax = abs(LonMaxOriginal-LonMinOriginal)
+
+#Return the length of the longest side 
+#to bound the expansion of search boxes
 if latmax>lonmax:
     maxdist = latmax
 else:
@@ -115,7 +115,7 @@ while dist<maxdist:
             #search an area just large enough to contain the largest running segments
             bounds = [LatMin, LonMin, LatMin+dist, LonMin+dist]
             boundfiletracker(starttime, workingdir, bounds, extent, dist)
-            #print 'sleep'
+            
             if calls >= 30000:
                 timedif = datetime.datetime(starttime.year, starttime.month, starttime.day+1)-starttime
                 if timedif.total_seconds() < 0:
@@ -135,43 +135,47 @@ while dist<maxdist:
                     if item and str(item.id) not in MBSStrava:
                         SegmentID = item.id #unique ID number
                         print 'New segment ID: ', SegmentID
-                        #segmentlist.add(SegmentID)
+                        
                         SegmentName = item.name #segment name
                         calls +=1
-                        #print SegmentName
+                        
                         try:
                             SegDetails = client.get_segment(SegmentID)
                             usetype = SegDetails.activity_type
-                            #print calls
-                            creationdate = SegDetails.created_at #Date the segment was uploaded to strava
+                            
+                            #Date the segment was uploaded to strava
+                            creationdate = SegDetails.created_at 
                             creationdate = creationdate.replace(tzinfo=None)
-                            #print 'creation date ', creationdate
-                            #print creationdate.strftime('%Y%m%d'),type(creationdate.strftime('%Y%m%d'))
-                            effortcount = SegDetails.effort_count #efforts between creation date and collection date
-                            #print 'effort count' , effortcount
-                            athletecount = SegDetails.athlete_count #number of athletes who have tried the segment
+
+                            #efforts between creation date and collection date
+                            effortcount = SegDetails.effort_count
+
+                            #number of athletes who have tried the segment
+                            athletecount = SegDetails.athlete_count
+
+                            #Time between creation and current
                             timedelt = starttime-creationdate
                             TimeDelt = timedelt.total_seconds()
                             yrs = TimeDelt/(60*60*24*365) #convert seconds to years
-                            #print yrs, type(yrs)
+                            
                             if yrs:
                                 userate = float(effortcount)/yrs #Efforts per year as 'userate'
                                 
-                                #print userate
+                                
                             else:
                                 userate = 0.0
-                            #print 'use rate ', userate
+                            
                             line  = SegDetails.map.polyline
-                            #print line
-                            shape = polyline.decode(line) #google polyline encoding as a string decode with polyline lib
-                            #print shape                        
+                            
+                            #google polyline encoding as a string decode with polyline lib
+                            shape = polyline.decode(line)
+                                                   
                             if shape:
                                 wkt = ogr.Geometry(ogr.wkbLineString)
                                 for item in shape:
                                     wkt.AddPoint(item[1],item[0])
-                                #Create a list with all attributes.
-                                #info = [, SegmentID, creationdate.strftime('%Y%m%d'), int(effortcount), int(athletecount), yrs, userate, wkt]
-                                
+                                #Create a dictionary with all attributes.
+                                                                
                                 MBSStrava[str(SegmentID)]={'name': SegmentName,
                                                            'segmentID': int(SegmentID), 
                                                            'usetype': usetype,
@@ -252,6 +256,7 @@ while dist<maxdist:
             print bounds
             print datetime.datetime.now()
             boundfiletracker(starttime, workingdir, bounds, extent, dist)
+    #Double the distance and repeat until the distance exceeds the length of the longer side.
     dist = dist*2
     LatMin = LatMinOriginal
     LonMin = LonMinOriginal
